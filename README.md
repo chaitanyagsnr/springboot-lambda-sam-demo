8ff10fae89f8953c2293fa8299fd594574282674
Chaitanya G S N R <chaitanya.gsnr@outlook.com>
2023-10-27 12:46:14 +0530

    Adding support for multiple environments

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ Python, AWS CLI and AWS SAM CLI have to be installed. An active AWS account is n
 `sam build --template cloudformation/sam-template.yml`
 
 ### Deploy
-`sam deploy --guided`
+`sam deploy --config-env <ENV_NAME>`
+
+Env Name can be `default`, `dev`, `qa` or `prod`
 
 ### How to verify deployment
 `curl <API Gateway Invoke URL>/hello`
--- src/main/java/com/example/samdemo/controller/HelloController.java
+++ src/main/java/com/example/samdemo/controller/HelloController.java
@@ -1,18 +1,23 @@
 package com.example.samdemo.controller;
 
 import com.example.samdemo.dto.MessageDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/hello")
 public class HelloController {
+
+    @Value(value = "${constants.message}")
+    private String message;
+
     @GetMapping
     public String hello() {
-        return "Hello World";
+        return message;
     }
 
     @PostMapping(path = "/save/{name}")
     public String save(@PathVariable(name = "name") String str, @RequestBody MessageDto messageDto) {
-        return "Hello " + str + ". "+ messageDto.getMessage();
+        return message + " " + str + ". "+ messageDto.getMessage();
     }
 }
